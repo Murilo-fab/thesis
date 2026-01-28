390fa4aab6c8494c14bebf3bc793b6ff02dd0d1b
Murilo-fab <murilox26@hotmail.com.br>
2026-01-28 11:17:48 +0100

    New classification head

--- src/thesis/downstream_models.py
+++ src/thesis/downstream_models.py
@@ -6,19 +6,29 @@ class ClassificationHead(nn.Module):
     def __init__(self, input_dim, num_classes):
         
         super().__init__()
+        # self.classifier = nn.Sequential(
+        #     nn.Linear(input_dim, 512),  
+        #     nn.BatchNorm1d(512),       
+        #     nn.ReLU(),                 
+        #     nn.Dropout(0.1),           
+        #     nn.Linear(512, 256),       
+        #     nn.BatchNorm1d(256),       
+        #     nn.ReLU(),                 
+        #     nn.Dropout(0.1),           
+        #     nn.Linear(256, 128),       
+        #     nn.BatchNorm1d(128),       
+        #     nn.ReLU(),                 
+        #     nn.Linear(128, num_classes) 
+        # )
         self.classifier = nn.Sequential(
-            nn.Linear(input_dim, 512),  
-            nn.BatchNorm1d(512),       
-            nn.ReLU(),                 
-            nn.Dropout(0.1),           
-            nn.Linear(512, 256),       
-            nn.BatchNorm1d(256),       
-            nn.ReLU(),                 
-            nn.Dropout(0.1),           
-            nn.Linear(256, 128),       
-            nn.BatchNorm1d(128),       
-            nn.ReLU(),                 
-            nn.Linear(128, num_classes) 
+            nn.Linear(input_dim, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Linear(128, num_classes)
         )
 
     def forward(self, x):
